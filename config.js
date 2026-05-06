2883acca642b78cd80aa33d63cda460fe7370a0d
mdshahavuddinm904 <mdshahavuddinm@gmail.com>
2026-05-06 22:31:01 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "8663292414:AAHmYPxUMcCBbgGzw53DcXBZ-RERI5xdDKY",
+  BOT_TOKEN: "8661744403:AAE18iXxnlw_WUvPulmZQRxjZlseZhmsVPY",
   ADMIN_ID: 8136997138,
   CHANNEL: "@Global_Method_Channel"
 };
