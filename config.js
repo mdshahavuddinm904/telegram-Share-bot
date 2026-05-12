ec265808aac58edbd2da7c2d07b1575663ec43e3
mdshahavuddinm904 <mdshahavuddinm@gmail.com>
2026-05-13 02:30:44 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "8709620585:AAF54hJzZ5HZrzRwnKDR3hiWBI6Baqdr8Ho",
+  BOT_TOKEN: "8709620585:AAFdGdwSuqLEY29SWyawApItx0C_EVqMIuM",
   ADMIN_ID: 8136997138,
   CHANNEL: "@MiniDemoUpdate"
 };
